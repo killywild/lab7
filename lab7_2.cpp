e37675d046a63ad419bc0cb89d308f61c7677e0f
killywild <skyguysky002@gmail.com>
2022-12-27 14:16:58 +0700

    fin

--- lab7_2.cpp
+++ lab7_2.cpp
@@ -4,11 +4,31 @@ using namespace std;
 
 int main(){
 	
-	int i = 0, P[5000], k, result;
-	while(i < 5000){
-		P[i] = 2*i+1;
-		i++;
+	int i = 0;
+	int k, result;
+	int P[5000];
+	cout << "Please input k: ";
+	cin >> k;
+	if (k > 0)
+	{
+		result = 0;
+		while(i < 5000)
+		{
+			P[i] = 2*i+1;
+			if(P[i]%k == 0)
+			{
+				result -= P[i];
+			}
+			else
+			{
+				result += P[i];
+			}
+			i++;
+		}
+		cout << "Result = " << result << endl;
+		}else{
+		cout << "Invalid input!!!";
 	}
-	
+
 	return 0;
 }
